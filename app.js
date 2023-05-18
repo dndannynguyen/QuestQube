72d6fad4289beb33b4da787d024f84dbc70a2fc5
MARTIN SIU <msiu28@my.bcit.ca>
2023-05-17 20:24:40 -0700

    Create function to upload photo to cloud bucket

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userModel = require('./models/user')
 const bcrypt = require('bcrypt')
 const joi = require('joi')
 const app = express()
+const crypto = require('crypto');
 require('dotenv').config();
 require("./utils.js");
 const verifyGame = require('./verifyGame.js');
@@ -28,6 +29,7 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 backblaze_account = process.env.BACKBLAZE_ACCOUNT_ID;
 backblaze_API = process.env.BACKBLAZE_APPLICATION_KEY;
 backblaze_bucket = process.env.BACKBLAZE_BUCKET_ID;
+backblaze_name = process.env.BACKBLAZE_BUCKET_NAME;
 
 var {
     database
@@ -46,18 +48,59 @@ const b2 = new B2({
     applicationKey: backblaze_API
   });
 
-  async function GetBucket() {
+  async function GetBucketAndUploadPhoto() {
     try {
-      await b2.authorize(); // must authorize first (authorization lasts 24 hrs)
-      let response = await b2.getBucket({ bucketName: backblaze_bucket });
+      await b2.authorize();
+      let response = await b2.getBucket({ bucketName: backblaze_name });
       console.log(response.data);
-      console.log("success")
+      console.log("Success! Backblaze server is connected.");
+  
+      // Define the folder name
+      const folderName = 'user';
+  
+      // Upload a photo to the bucket in the specified folder
+      const photoPath = 'C:/Users/marti/Documents/2800-202310-DTC10/public/mediaResources/avatars/1.jpg';
+      const fileName = folderName + '/uploaded-photo.jpg';
+  
+      fs.readFile(photoPath, async (err, fileData) => {
+        if (err) {
+          console.log('Error reading photo:', err);
+          return;
+        }
+  
+        try {
+          const uploadUrlResponse = await b2.getUploadUrl({ bucketId: backblaze_bucket });
+          const uploadUrl = uploadUrlResponse.data.uploadUrl;
+          const uploadAuthToken = uploadUrlResponse.data.authorizationToken;
+          console.log('Got an upload URL:', uploadUrl);
+          console.log('Got an upload auth token:', uploadAuthToken);
+
+          // Calculate the SHA1 hash of the file content
+        const contentSha1 = crypto.createHash('sha1').update(fileData).digest('hex');
+  
+          // Call the uploadFile method with the obtained upload URL and auth token
+          await b2.uploadFile({
+            uploadUrl: uploadUrl,
+            uploadAuthToken: uploadAuthToken,
+            fileName: fileName,
+            data: fileData,
+            bucketName: backblaze_name,
+            contentSha1: contentSha1
+          });
+          console.log('Photo uploaded successfully:', fileName);
+  
+        } catch (error) {
+          console.error('Error uploading photo:', error);
+        }
+      });
     } catch (err) {
       console.log('Error getting bucket:', err);
     }
   }
+  
+  // Call the function to get the bucket and upload the photo when the page loads
+  GetBucketAndUploadPhoto();
 
-  GetBucket();
 
 var mongoStore = MongoStore.create({
     mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
